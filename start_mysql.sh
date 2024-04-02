4f2dbcbfbdd3028e575addf5d1c67c2b2799a3e4
edurekavivekh <vivek.hebalkar@gmail.com>
2024-04-02 10:20:00 +0000

    database automation added

--- start_mysql.sh
+++ start_mysql.sh
@@ -1,3 +1,4 @@
-#docker run -d --name database -p 3306:3306 -e MYSQL_ROOT_PASSWORD=pass@123 -d mysql:latest
+docker run -d --name database -p 3306:3306 -e MYSQL_ROOT_PASSWORD=pass@123 -d mysql:latest
+sleep 60s
 mysql -u'root' -p'pass@123' -h 127.0.0.1 < setup_mysql.sql
 
